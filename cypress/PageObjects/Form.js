dfeb2f14ddf696832838b24acb7a5a9dea43f616
murty-555 <krlvmurthy96@gmail.com>
2024-07-24 18:53:00 +0530

    my task on cypress

---
+++ cypress/PageObjects/Form.js
@@ -0,0 +1,68 @@
+class Form {
+    clickFormCategory(){
+        cy.get(".category-cards>:nth-child(2)").click()
+    }
+
+    clickPracticeForm(){
+        cy.get(".left-pannel>.accordion>:nth-child(2)>.element-list").click()
+    }
+
+    setFirstName(firstName){
+        cy.get('#userName-wrapper>:nth-child(2)>#firstName').type(firstName)
+    }
+
+    setLastName(lastName){
+        cy.get('#userName-wrapper>:nth-child(4)>#lastName').type(lastName)
+    }
+
+    setEmail(email){
+        cy.get('#userEmail-wrapper>:nth-child(2)>#userEmail').type(email)
+    }
+
+    selectingRadioButton(gender){
+        cy.get("#gender-radio-1").check(gender, {force: true})
+    }
+
+    setMobileNumber(number){
+        cy.get('#userNumber').type(number)
+    }
+
+    setDateOfBirth(dateOfBirth){
+        cy.get("#dateOfBirthInput").click()
+        cy.get(".react-datepicker__year-select").select(dateOfBirth.year)
+        cy.get(".react-datepicker__month-select").select(dateOfBirth.month)
+        cy.get(".react-datepicker__day.react-datepicker__day--009.react-datepicker__day--weekend").click()
+    }
+
+    setSubjects(subjects){
+        cy.get("#subjectsInput").type(subjects)
+    }
+
+    selectCheckBoxes(){
+        cy.get("#hobbies-checkbox-1").check({force:true})
+        cy.get("#hobbies-checkbox-3").check({force:true})
+    }
+
+    fileUpload(){
+        cy.get("#uploadPicture").attachFile("photo.jpeg")
+    }
+
+    setCurrentAddress(currentAddress){
+        cy.get("#currentAddress-wrapper>:nth-child(2)>textarea").type(currentAddress)
+    }
+
+    submitForm(){
+        cy.get("#submit").click()
+    }
+
+    VerifyFormSubmission(expectedResult){
+        cy.get("#example-modal-sizes-title-lg").should('have.text', expectedResult)
+    }
+
+    TakeScreenshot(){
+        cy.get(".fade.modal-backdrop.show").screenshot()
+    }
+    
+}
+
+export default Form
---
+++ cypress/e2e/MyTask.cy.js
@@ -0,0 +1,80 @@
+import "cypress-file-upload"
+
+import Form from "../PageObjects/Form";
+
+describe("Test suite", () => {
+
+    //for handling uncaught exception error
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from failing the test
+        return false;
+      });
+
+    let userData;
+
+    beforeEach("getting fixtures data",() => {
+        cy.fixture("formData").then((data) => {
+            userData = data
+        })
+    })
+    
+    it("working on form", () => {
+        //visiting the page
+        cy.visit("https://demoqa.com/")
+
+        let form = new Form()
+
+        //clicking on the forms category
+        form.clickFormCategory()
+
+        //clicking on practice form
+        form.clickPracticeForm()
+
+        //accessing and assigning user firstname input
+        form.setFirstName(userData.firstName)
+
+        //accessing and assigning user lastname input
+        form.setLastName(userData.lastName)
+
+        //accessing and assigning user email
+        form.setEmail(userData.email)
+
+        //selecting gender using radio buttons
+        form.selectingRadioButton(userData.gender)
+
+        //accessing mobile number
+        form.setMobileNumber(userData.mobileNumber)
+
+        //accessing and selecting date
+        form.setDateOfBirth(userData.dateOfBirth)
+
+        // accessing and assigning user subjects
+        form.setSubjects(userData.subjects)
+
+        //accessing and selecting check boxes
+        form.selectCheckBoxes()
+
+        //accessing and working with file upload
+        form.fileUpload()
+        // cy.wait(5000)
+
+        //accessing and assigning user current address
+        form.setCurrentAddress(userData.currentAddress)
+
+        //accessing and working on dropdowns
+        // cy.get(".css-1g6gooi>div>#react-select-3-input")
+        // cy.get("#react-select-4-input")
+
+        //submitting the form
+        form.submitForm()
+
+        //checking for final result
+        form.VerifyFormSubmission(userData.expectedResult)
+
+        //taking screenshot
+        form.TakeScreenshot()
+
+    })
+
+
+})
